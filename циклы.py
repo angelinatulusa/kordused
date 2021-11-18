9110688814aa8c4a341f24739b802488aa28c93a
opilane <opilane@HP-CZC6297BB4>
2021-11-18 16:26:46 +0200

    Задачи по теме IF,While, For и проверка вводимых данных

--- циклы.py
+++ циклы.py
@@ -1,3 +1,126 @@
+#5. Составьте программу для вычисления площади трапеции.
+t=int(input("ho4es naiti plosad trapecii?(da-1) ")) 
+if t==1:
+    a=0.0
+    while a<=0:
+        try:
+            a=int(input("cemu ravna storona a? "))
+        except ValueError:
+            print("tolko cisla bolse cem 0")
+    b=0.0
+    while b<=0:
+        try:
+            b=int(input("cemu ravna storona b? "))
+        except ValueError:
+            print("tolko cisla bolse cem 0")
+    h=0.0
+    while h<=0:
+        try:
+            h=int(input("cemu ravna visota h? "))
+        except ValueError:
+            print("tolko cisla bolse cem 0")
+    S=0.5*(a+b)*h
+    print("plosad ravna - ",S)
+    q=input("hotes ese? ")
+    if q!="da": quit()
+else:
+    print("horoso, dosvidania!")
+
+
+print()
+print()
+print()
+#primer
+from keyboard import *
+while True:
+    print("Hello!")
+    if is_pressed("esc"): break
+
+
+print()
+print()
+print()
+#28. Реализуйте "мини лотерею". Пусть компрьютер "задумает число", а пользователь его должен отгадать. В конце сообщив количество попыток.
+from random import *
+b=randint(1,10)
+a=int(input("Kakoe cislo? "))
+for b in range(1,10):
+    if a==b:
+        print("Pozdravlau, vi ugadali cislo!")
+        break
+    else:
+        int(input("Neverno, poprobuite ese raz. Kakoe cislo? "))
+
+print()
+print()
+print()
+#4.  Имеется кусок ткани длиной М метров. От него последовательно отрезаются куски разной длины. Все данные по использованию ткани заносятся в компьютер. Компьютер должен выдать сообщение о том, что материала не хватает, если будет затребован кусок ткани, большей длины, чем имеется и предложить выкупить остаток. Если пользователь согласен, то продается последний кусок и программа заканчивает работу, если нет, то переходим к следующиму покупателю.
+from random import *
+M=randint(10,100)
+print("v magazine",M,"m tkani")
+while M>0:
+    m=int(input("kakoi kusok hotite? "))
+    if M>=m:
+        M-=m
+        print("u nas ostalos",M,"m")
+    else:
+        print("u nas nehvataet tkani")
+        v=input("hotite kupit ostatok? ")
+        if v=="da":
+            print("tkan vasa")
+            M=0
+        else:
+            print("ne hotes, kak hotes")
+print("v magazine zakoncilas tkan",M,"m")
+    
+
+
+
+print()
+print()
+print()
+#3. Начав тренировки, спортсмен в первый день пробежал 10 км. Каждый день он увеличивал дневную норму на 10% нормы предыдущего дня. Какой суммарный путь пробежит спортсмен за 7 дней?
+km=s_pikkus=10 #km v pervii den
+print("1. v den put bil ", km)
+for d in range(2,8):
+    km*=1.1
+    print(d,". v den put bil ",round(km,2))
+    s_pikkus+=km
+    print(d,". v den summarnii put bil ",round(s_pikkus,2))
+
+
+
+print()
+print()
+print()
+#1. Определить и вывести максимальное из N вводимых пользователем чисел.
+ask_P=ask_N=""
+while type(ask_P)!=int:
+    try:
+        ask_P=int(input("skolko cisel vi hotite vvesti? "))
+    except ValueError:
+        print("Ainult täisarvud!")
+if ask_P>0:
+    while type(ask_N)!=int:
+        ask_N=int(input("kakoe cislo vvodim?"))
+        print(" ")
+    ask_P-=1
+    maks=ask_N
+while ask_P>0:
+    ask_N=int(input("kakoe cislo vvodim?"))
+    ask_P-=1
+if ask_N>maks: 
+    maks=ask_N
+    print("maksimalnoe cislo eto ", maks)
+else:
+    print("ne mogu naiti maksimalnoe cislo")
+
+#Задачи по теме IF,While, For и проверка вводимых данных
+
+
+print()
+print()
+print()
 #28. Реализуйте "мини лотерею". Пусть компрьютер "задумает число", а пользователь его должен отгадать. В конце сообщив количество попыток.
 from random import *
 b=randint(1,10)
